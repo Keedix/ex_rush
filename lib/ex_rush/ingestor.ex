e41b6b5e1691e1273e8466b25d14db5c9e4bd95b
auklejewski <arturuklejewski@rezolve.com>
2021-06-01 17:31:15 +0200

    Cleanup ingestor

--- lib/ex_rush/ingestor.ex
+++ lib/ex_rush/ingestor.ex
@@ -22,26 +22,31 @@ defmodule ExRush.Ingestor do
       |> File.read!()
       |> Jason.decode!()
       |> Enum.each(fn statistic ->
-        Statistics.create_statistic(%{
-          player: statistic["Player"],
-          position: statistic["Pos"],
-          rushing_20: statistic["20+"],
-          rushing_40: statistic["40+"],
-          rushing_attempts: statistic["Att"],
-          rushing_attempts_per_game_average: statistic["Att/G"],
-          rushing_average_yards_per_attempt: statistic["Avg"],
-          rushing_first_down_percentage: statistic["1st%"],
-          rushing_first_downs: statistic["1st"],
-          rushing_fumbles: statistic["FUM"],
-          rushing_yards_per_game: statistic["Yds/G"],
-          team: statistic["Team"],
-          total_rushing_touchdowns: statistic["TD"],
-          total_rushing_yards: statistic["Yds"],
-          longest_rush: statistic["Lng"]
-        })
-        |> IO.inspect()
+        statistic
+        |> format_params()
+        |> Statistics.upsert_statistic!()
       end)
 
     {:noreply, path}
+  end
+
+  defp format_params(statistic) do
+    %{
+      player: statistic["Player"],
+      position: statistic["Pos"],
+      rushing_20: statistic["20+"],
+      rushing_40: statistic["40+"],
+      rushing_attempts: statistic["Att"],
+      rushing_attempts_per_game_average: statistic["Att/G"],
+      rushing_average_yards_per_attempt: statistic["Avg"],
+      rushing_first_down_percentage: statistic["1st%"],
+      rushing_first_downs: statistic["1st"],
+      rushing_fumbles: statistic["FUM"],
+      rushing_yards_per_game: statistic["Yds/G"],
+      team: statistic["Team"],
+      total_rushing_touchdowns: statistic["TD"],
+      total_rushing_yards: statistic["Yds"],
+      longest_rush_with_touchdown: statistic["Lng"]
+    }
   end
 end
